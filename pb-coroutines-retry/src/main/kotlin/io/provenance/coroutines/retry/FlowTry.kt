187cf97609f5c250ef47128e72b1121bb911d9a4
Phil Story <mtps@users.noreply.github.com>
2022-06-21 16:20:16 -0600

    Move generic tryOnEach to correct location.

---
+++ pb-coroutines-retry/src/main/kotlin/io/provenance/coroutines/retry/FlowTry.kt
@@ -0,0 +1,22 @@
+package io.provenance.coroutines.retry
+
+import io.provenance.coroutines.retry.flow.FlowProcessor
+import io.provenance.coroutines.tryOnEach
+import kotlinx.coroutines.flow.Flow
+import mu.KotlinLogging
+
+/**
+ * Wrap [onEach] into a try {} catch {} to allow dropping the failed flow element into a [FlowProcessor] for later reprocessing.
+ *
+ * @param flowProcessor The [FlowProcessor] containing callbacks for processing and error handling.
+ * @return The original flow.
+ */
+fun <T> Flow<T>.tryOnEachProcess(flowProcessor: FlowProcessor<T>): Flow<T> = tryOnEach(
+    onFailure = { it, e ->
+        KotlinLogging.logger {}.warn("failed to process record", e)
+        flowProcessor.send(it, e)
+    },
+    tryBlock = {
+        flowProcessor.process(it)
+    }
+)
