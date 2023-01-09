4314ac6a9644b46ac2a31a3121d2b40ced203bfa
Phil Story <mtps@users.noreply.github.com>
2023-01-09 10:35:42 -0700

    Fix tests to run correctly

--- ft-coroutines-core/src/test/kotlin/tech/figure/coroutines/flows/FlowChunkTest.kt
+++ ft-coroutines-core/src/test/kotlin/tech/figure/coroutines/flows/FlowChunkTest.kt
@@ -2,6 +2,7 @@ package tech.figure.coroutines.flows
 
 import io.kotest.core.spec.style.AnnotationSpec
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.flowOf
@@ -11,19 +12,20 @@ import mu.KotlinLogging
 class FlowChunkTest : AnnotationSpec() {
     private val log = KotlinLogging.logger("tester")
 
-    @Test
-    fun testEmptyFlowChunked() = runBlocking {
+    @Test(CancellationException::class)
+    fun testEmptyFlowChunked(): Unit = runBlocking {
         val f = flowOf<Int>().cancellable()
 
         f.chunked(10, 5.seconds).collect {
             log.info("got:${it.toList()}")
             assert(it.isEmpty())
-            cancel()
+            cancel(CancellationException())
         }
+        error("shouldn't be reached")
     }
 
-    @Test
-    fun testIncompleteChunked() = runBlocking {
+    @Test(CancellationException::class)
+    fun testIncompleteChunked(): Unit = runBlocking {
         val f = flowOf(1, 2, 3, 4, 5).cancellable()
 
         var i = 0
@@ -31,8 +33,9 @@ class FlowChunkTest : AnnotationSpec() {
             log.info("got:${it.toList()}")
             assert(it.isNotEmpty())
             if (++i == 3) {
-                cancel()
+                cancel(CancellationException())
             }
         }
+        error("shouldn't be reached")
     }
 }
