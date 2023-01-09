ab03aa242ce53f0dd5aaa3df2be7a99ce5371fbc
Phil Story <mtps@users.noreply.github.com>
2023-01-09 09:41:07 -0700

    Upgrade versions of dependencies

--- ft-coroutines-core/build.gradle.kts
+++ ft-coroutines-core/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
     id("core-config")
 }
+
+dependencies {
+    implementation(libs.kotlinLogging)
+    implementation(libs.bundles.logback)
+    implementation("org.slf4j:slf4j-api:2.0.5")
+}
