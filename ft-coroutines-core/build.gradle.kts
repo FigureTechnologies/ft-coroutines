2ee7426bacd6ee42426c2d2f258326906ded6dd8
Phil Story <mtps@users.noreply.github.com>
2023-01-09 17:36:53 -0700

    Add chunking (#46)
    * Add chunking
    * Fix tests to run correctly

--- ft-coroutines-core/build.gradle.kts
+++ ft-coroutines-core/build.gradle.kts
@@ -6,4 +6,6 @@ dependencies {
     implementation(libs.kotlinLogging)
     implementation(libs.bundles.logback)
     implementation(libs.slf4j.api)
+
+    testImplementation(libs.kotlinx.coroutines.core)
 }
